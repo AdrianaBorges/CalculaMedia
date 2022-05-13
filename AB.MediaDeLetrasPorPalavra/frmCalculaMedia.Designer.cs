09186000bcee55046c23e3fb7e593997528eaeae
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-05-13 01:32:32 -0300

    first commit

---
+++ AB.MediaDeLetrasPorPalavra/frmCalculaMedia.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace AB.MediaDeLetrasPorPalavra
+{
+    partial class frmCalculaMedia
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtCalcular = new System.Windows.Forms.Button();
+            this.txtParametro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLimpar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtLimpar);
+            this.panel1.Controls.Add(this.lblResult);
+            this.panel1.Controls.Add(this.txtCalcular);
+            this.panel1.Controls.Add(this.txtParametro);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(5, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(424, 121);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(7, 83);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(71, 17);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "lblResult";
+            // 
+            // txtCalcular
+            // 
+            this.txtCalcular.Location = new System.Drawing.Point(324, 55);
+            this.txtCalcular.Name = "txtCalcular";
+            this.txtCalcular.Size = new System.Drawing.Size(93, 23);
+            this.txtCalcular.TabIndex = 2;
+            this.txtCalcular.Text = "Efetuar Calculo";
+            this.txtCalcular.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.txtCalcular.UseVisualStyleBackColor = true;
+            this.txtCalcular.Click += new System.EventHandler(this.txtCalcular_Click);
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtParametro.Location = new System.Drawing.Point(6, 28);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(411, 20);
+            this.txtParametro.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe o texto para calculo:";
+            // 
+            // txtLimpar
+            // 
+            this.txtLimpar.Location = new System.Drawing.Point(324, 85);
+            this.txtLimpar.Name = "txtLimpar";
+            this.txtLimpar.Size = new System.Drawing.Size(93, 23);
+            this.txtLimpar.TabIndex = 4;
+            this.txtLimpar.Text = "Limpar Tela";
+            this.txtLimpar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.txtLimpar.UseVisualStyleBackColor = true;
+            this.txtLimpar.Click += new System.EventHandler(this.txtLimpar_Click);
+            // 
+            // frmCalculaMedia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 141);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCalculaMedia";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcula Média de Letras por palavras";
+            this.Load += new System.EventHandler(this.frmCalculaMedia_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button txtCalcular;
+        private System.Windows.Forms.TextBox txtParametro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button txtLimpar;
+    }
+}
+
---
+++ AB.MediaDeLetrasPorPalavra/frmCalculaMedia.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AB.MediaDeLetrasPorPalavra
+{
+    public partial class frmCalculaMedia : Form
+    {
+        public frmCalculaMedia()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalculaMedia_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = string.Empty;
+        }
+
+        private static int GetMedia(List<Texto> lstPalavras)
+        {
+            var totMedia = 0;
+
+            foreach (var item in lstPalavras)
+            {
+                totMedia += item.TotLetras;
+            }
+
+            return totMedia / lstPalavras.Count;
+
+        }
+
+        public static List<Texto> CarregaLista(string texto)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+
+            string[] palavras = texto.Split(delimiterChars);
+
+            var lstTexto = new List<Texto>();
+
+            foreach (var item in palavras)
+            {
+                lstTexto.AddRange(new[] { new Texto(item.Length, item) });
+            }
+
+            return lstTexto;
+
+        }
+
+        private void txtCalcular_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = $"Média de letras por palavra = " + GetMedia(CarregaLista(txtParametro.Text));
+        }
+
+        private void txtLimpar_Click(object sender, EventArgs e)
+        {
+            txtParametro.Text = string.Empty;
+            lblResult.Text = string.Empty;
+
+        }
+
+        #region "Classe responsável pelo Texto"
+        public class Texto
+        {
+            public int TotLetras { get; set; }
+            public string Palavra { get; set; } = string.Empty;
+            public Texto() { }
+
+            public Texto(int totLetras, string palavra)
+            {
+                TotLetras = totLetras;
+                Palavra = palavra;
+            }
+
+        }
+
+        #endregion
+
+    }
+}
